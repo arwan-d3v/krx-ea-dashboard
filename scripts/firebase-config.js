3a370494dabdc2dab1def595a16c0c7914350025
arwan-d3v <arwan.d3v@gmail.com>
2026-05-25 09:21:49 +0800

    add firebase-config.js for VPS scripts

---
+++ scripts/firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase Web App Configuration
+// Used by KRX EA Dashboard frontend (Next.js)
+// Copy this file to VPS along with other scripts
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaSpsr-1RSyK0sUtDTp9uGkapwb-vXJA0",
+  authDomain: "krx-modern-dev.firebaseapp.com",
+  databaseURL: "https://krx-modern-dev-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "krx-modern-dev",
+  storageBucket: "krx-modern-dev.firebasestorage.app",
+  messagingSenderId: "225123976501",
+  appId: "1:225123976501:web:9c205ef34e9aa36391387c"
+};
+
+module.exports = firebaseConfig;
